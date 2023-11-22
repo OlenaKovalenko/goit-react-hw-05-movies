edbf3b2a3ab59495ae969b0f72608c2f3379bf52
OlenaKovalenko <elenkow17@gmail.com>
2023-11-22 22:00:34 +0200

    Create api.js

---
+++ src/api.js
@@ -0,0 +1,16 @@
+const API_KEY = '22b424da6b6918c86f924199d993847b';
+const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMmI0MjRkYTZiNjkxOGM4NmY5MjQxOTlkOTkzODQ3YiIsInN1YiI6IjY1NWU1NzdmMWRiYzg4MDBjNjg3YjdiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7JiqbArn44w22LWZzArm9OfmTh0GBCl_DykVvj7S4Tw';
+
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMmI0MjRkYTZiNjkxOGM4NmY5MjQxOTlkOTkzODQ3YiIsInN1YiI6IjY1NWU1NzdmMWRiYzg4MDBjNjg3YjdiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7JiqbArn44w22LWZzArm9OfmTh0GBCl_DykVvj7S4Tw'
+  }
+};
+
+fetch('https://api.themoviedb.org/3/authentication', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
