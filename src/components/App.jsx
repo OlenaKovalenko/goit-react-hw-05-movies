c48aad918108a1af5cb1cb82dbd7403878a87ddb
OlenaKovalenko <elenkow17@gmail.com>
2023-11-25 13:52:17 +0200

    add pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import NotFound from "path/to/pages/NotFound";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
